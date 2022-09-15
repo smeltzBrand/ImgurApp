ad8e5683dc1b014114e1b8ceb975df04d710762c
smeltzBrand <101213259+smeltzBrand@users.noreply.github.com>
2022-09-15 09:58:15 -0400

    Add project files.

---
+++ ImgurApp/Controllers/PhotoController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using ImgurApp.Models;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ImgurApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+
+        static HttpClient client = new HttpClient();
+        string path = "http://api.imgur.com/3";
+
+        [HttpGet]
+        public async Task<List<ImgurApp.Models.Image>> Get()
+        {
+
+            List<ImgurApp.Models.Image> images = null;
+
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                images = JsonSerializer.Deserialize<List<ImgurApp.Models.Image>>(jsonResponse);
+
+            }
+            return images;
+
+        }
+
+        [HttpPost]
+        public async Task<Uri> Post(ImgurApp.Models.Image image)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                 "/upload", image);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<Uri> Delete(int id)
+        {
+            HttpResponseMessage response = await client.PostAsJsonAsync(
+                 "/delete", id);
+            response.EnsureSuccessStatusCode();
+
+            // return URI of the created resource.
+            return response.Headers.Location;
+        }
+
+
+    }
+}
---
+++ ImgurApp/Models/Image.cs
@@ -0,0 +1,10 @@
+﻿namespace ImgurApp.Models
+{
+    public class Image
+    {
+        public int id { get; set; }
+        public IFormFile Photo { get; set; }
+        public string PhotoFileName { get; set; }
+        public string Description { get; set; }
+    }
+}
